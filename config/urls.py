d33f3391edd5af5f9000b49f90b63dcdcae1594c
abdullahiadow3059-a11y <abdullahiadow3059@gmail.com>
2026-04-25 06:30:26 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # Make sure include is imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), # This connects everything
+    path('', include('core.urls')), # Your app urls
+    path('accounts/', include('django.contrib.auth.urls')), # <--- ADD THIS LINE
 ]
